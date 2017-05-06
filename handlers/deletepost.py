87df756c5ac8811c200841a857ec7f0c4044a6f4
Jormangandur <=>
2017-05-06 18:50:35 +0100

    add decorator validations to deletepost, include login/logout in header

--- handlers/deletepost.py
+++ handlers/deletepost.py
@@ -7,14 +7,14 @@ from google.appengine.ext import db
 
 
 class DeletePostHandler(BlogHandler):
-    def get(self, post_id):
-        post = BlogPost.by_id(post_id)
+    @BlogHandler.user_logged_in
+    @BlogHandler.post_exists
+    @BlogHandler.user_owns_post
+    def get(self, post):
+        post_id = post.key().id()
         likes = Like.by_post_id(post_id)
         comments = Comment.by_post_id(post_id)
-        if self.user and post and self.user.key().id() == post.owner_id:
-            post.delete()
-            for like in likes:
-                like.delete()
-            for comment in comments:
-                comment.delete()
+        post.delete()
+        db.delete(likes)
+        db.delete(post)
         self.redirect('/blog')
